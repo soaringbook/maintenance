04e2c496552f618a53a6307aefa1c307fb93e3ba
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-29 08:42:05 +0200

    Add basic project setup.

---
+++ Maintenance/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Maintenance
+//
+//  Created by Jelle Vandenbeeck on 29/09/15.
+//  Copyright © 2015 Soaring Book. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
