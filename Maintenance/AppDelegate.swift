b56ca29f4e2357fe21cbdd66c6b28c474900934f
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-29 11:28:32 +0200

    Show login when needed.

--- Maintenance/AppDelegate.swift
+++ Maintenance/AppDelegate.swift
@@ -12,8 +12,36 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // MARK: - UIApplicationDelegate
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        presentSetupFlowIfNeeded()
+    }
+    
+    // MARK: - Flow
+    
+    private func presentSetupFlowIfNeeded() {
+        guard self.window?.rootViewController?.presentedViewController == nil else {
+            // Return when a modal is already being presented.
+            return
+        }
+        
+        if SBKeychain.sharedInstance.token != nil {
+            // Return when a token is set.
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        if let controller = storyboard.instantiateInitialViewController() {
+            // Use dispatch async to overcome the unbalanced warning.
+            dispatch_async(dispatch_get_main_queue()) {
+                self.window?.rootViewController?.presentViewController(controller, animated: false, completion: nil)
+            }
+        }
     }
 }
