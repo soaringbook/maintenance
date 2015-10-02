caeeca7a464bc95961d22f347db170561b58af05
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-10-02 16:55:18 +0200

    Add the glider model.

---
+++ Maintenance/Models/Glider.swift
@@ -0,0 +1,14 @@
+//
+//  Glider.swift
+//  Maintenance
+//
+//  Created by Jelle Vandenbeeck on 02/10/15.
+//  Copyright © 2015 Soaring Book. All rights reserved.
+//
+
+import RealmSwift
+
+class Glider: Object {
+    dynamic var immatriculation: String = ""
+    dynamic var name: String = ""
+}
