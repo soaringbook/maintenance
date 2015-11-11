eeba6e34063d3134234b7b6a4ad31d85758123cd
Jelle Vandebeeck <jelle@fousa.be>
2015-11-11 13:13:47 +0100

    Style the dashboard.

---
+++ Runway/Components/Views/Buttons/DashboardButton.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardButton.swift
+//  Runway
+//
+//  Created by Jelle Vandebeeck on 11/11/15.
+//  Copyright © 2015 Soaring Book. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DashboardButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
