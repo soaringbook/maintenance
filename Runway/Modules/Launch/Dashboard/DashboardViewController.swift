f99d779e74ba1dcd17d61b6cefbdd7ecb916cc1d
Jelle Vandebeeck <jelle@fousa.be>
2015-11-11 12:54:09 +0100

    Add basic dashboard controller.

---
+++ Runway/Modules/Launch/Dashboard/DashboardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardViewController.swift
+//  Runway
+//
+//  Created by Jelle Vandebeeck on 11/11/15.
+//  Copyright © 2015 Soaring Book. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController : UIViewController, UITextFieldDelegate {
+    
+    // MARK: - Status bar
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
