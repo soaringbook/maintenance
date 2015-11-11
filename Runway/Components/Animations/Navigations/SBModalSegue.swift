bb12fc9bce94980aae3ec0ddeb0cf29af0550cd0
Jelle Vandebeeck <jelle@fousa.be>
2015-11-11 14:19:00 +0100

    Add settings reference.

---
+++ Runway/Components/Animations/Navigations/SBModalSegue.swift
@@ -0,0 +1,21 @@
+//
+//  SBModalSegue.swift
+//  Runway
+//
+//  Created by Jelle Vandebeeck on 11/11/15.
+//  Copyright © 2015 Soaring Book. All rights reserved.
+//
+
+import UIKit
+
+class SBModalSegue: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
