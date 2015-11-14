680c1b93dfefbd6f1e78a5d2fb63612aba57a5a6
Jelle Vandebeeck <jelle@fousa.be>
2015-11-14 09:22:19 +0100

    Add basic winter controller.

---
+++ Runway/Modules/Winter/WinterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WinterViewController.swift
+//  Runway
+//
+//  Created by Jelle Vandebeeck on 14/11/15.
+//  Copyright © 2015 Soaring Book. All rights reserved.
+//
+
+import UIKit
+
+class WinterViewController: UIViewController {
+
+    // MARK: - Status bar
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
